973d1b5706338ee55b1dd567886415731e4934c2
Szilasi István <szilasii78@gmail.com>
2024-10-03 08:14:27 +0200

    add data endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,19 @@ const app =express();
 const port = 3000;
 const host = "localhost";
 
+const persons = [
+  {name:"Maci Laci", address:"Yellowstone"},
+  {name:"Bubu", address:"Yellowstone"},
+  {name:"Mekk Elek", address:"Budapest"},
+  {name:"Hack Elek", address:"Budapest"}
+]
+
+
 app.get('/macika',(req,res)=>{
     res.send('Hello haver!')
 })
 app.post('/data',(req,res) => {
-    res.send(JSON.stringify('Hello haver!'))
+    res.send(persons)
 })
 
 app.listen(port,() => { console.log(`A szerver a http://${host}:${port}-on fut`)});
